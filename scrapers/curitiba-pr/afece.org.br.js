f769d92933da9ff7aa86f195e57f32015063160f
dirceupj <dirceuu@gmail.com>
2021-10-23 03:36:09 -0300

    Change ORM From Typeorm To Sequelize

--- scrapers/curitiba-pr/afece.org.br.js
+++ scrapers/curitiba-pr/afece.org.br.js
@@ -1,21 +1,33 @@
-const typeorm = require('typeorm');
+const { Sequelize } = require('sequelize');
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+const database = require('../../db');
+const Organization = require('../../models/Organization');
+const Post = require('../../models/Post');
+
+Post.belongsTo(Organization);
+Organization.hasMany(Post);
+
+// create tables if not exists
+database.sync();
 
 const org_name = 'Afece';
 const news_list_url = 'https://afece.org.br/noticias/';
 
-const connection_config = require('../connection_config.js');
+
+
+// const connection_config = require('../connection_config.js');
 
 // initialize Organization in DB
-typeorm.createConnection(connection_config).then(async connection => {
-  let orgRepository = connection.getRepository("Org");
-  let org = await orgRepository.find({ name: org_name });
-  if (org.length == 0) {
-    var new_org = { name: org_name };
-    orgRepository.save(new_org);
-  }
-}).catch(error => console.log(error));
+// typeorm.createConnection(connection_config).then(async connection => {
+//   let orgRepository = connection.getRepository("Org");
+//   let org = await orgRepository.find({ name: org_name });
+//   if (org.length == 0) {
+//     var new_org = { name: org_name };
+//     orgRepository.save(new_org);
+//   }
+// }).catch(error => console.log(error));
 
 // axios(news_list_url).then(response => {
 //   const html = response.data;
